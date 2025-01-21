ea5b871d8dc27f56e232fde627624ce848a283a9
MindScape <MindScape00@users.noreply.github.com>
2025-01-20 22:45:19 -0700

    EL: Pickers & Load LibStub & NineSlice

---
+++ _retail_/Interface/AddOns/EpsilonLib/Pickers/IconPicker.lua
@@ -0,0 +1,217 @@
+-------------------------------------------------------------------------------
+--
+-- Icon picker interface.
+--
+-- Use EpsilonLibIconPicker_Open( returnFunc, closeOnClick, playSound ) to open.
+--
+-- @param returnFunc	If provided, clicking an icon will return its file path
+--						as a string to this function.
+--						If none provided, clicking an icon will instead set the 
+--						following global variables you can call instead:
+--
+--						EpsilonLibIconPicker.IconPath	string	The icon path.
+--
+-- @param closeOnClick	If true, the picker will close when an icon is clicked.
+-- @param playSound		If true, the picker will play a sound when an icon is
+--						clicked.
+--
+
+local LibRPMedia = LibStub:GetLibrary("LibRPMedia-1.0");
+
+EPSILONLIB_ICONS = {};
+
+-- Generate the default icon list using LibRPMedia.
+
+for index, name in LibRPMedia:FindAllIcons() do
+	EPSILONLIB_ICONS[#EPSILONLIB_ICONS + 1] = name;
+end
+
+local startOffset = 0
+local filteredList = nil
+
+local MAX_ICONS		 = 42;
+local ICONS_PER_LINE = 7;
+local ICON_SIZE	 = 32;
+local ICON_SPACING	 = 5;
+
+local function GetIconPath( button )
+	if not button or not button.pickerIndex then
+		return ""
+	end
+	
+	local list = filteredList or EPSILONLIB_ICONS;
+	local texture = list[ button.pickerIndex + startOffset ];
+	
+	return texture
+end
+
+-------------------------------------------------------------------------------
+-- When the icon picker first loads, set up the icon map.
+--
+function EpsilonLibIconPicker_OnLoad( self )
+	SetPortraitToTexture(self.portrait,"Interface/Icons/misc_rnrpaintbuttonup");
+	self.TitleText:SetText( "Icons" );
+		
+	self:SetClampedToScreen( true );
+	self:RegisterForDrag( "LeftButton" );
+	self:SetScript( "OnDragStart", self.StartMoving );
+	self:SetScript( "OnDragStop", self.StopMovingOrSizing );
+	
+    -- create icon map
+    self.icons = {};
+    for y = 0, (ICONS_PER_LINE - 1) do
+        for x = 0, (ICONS_PER_LINE - 1) do
+        local btn = CreateFrame( "Button", nil, self.selectorFrame, "EpsilonLibIconPickerButton" );
+        btn:SetPoint( "TOPLEFT", "EpsilonLibIconPickerInset", ICON_SIZE*x+ICON_SPACING, (-1*ICON_SIZE)*(y-ICON_SPACING) );
+        btn:SetSize( ICON_SIZE, ICON_SIZE );
+            
+        table.insert( self.icons, btn );
+        btn.pickerIndex = #self.icons;
+        end
+    end
+end
+
+-------------------------------------------------------------------------------
+-- When one of the icon buttons are clicked.
+--
+function EpsilonLibIconPickerButton_OnClick( self )
+	-- Return the icon path through the provided return function
+	if EpsilonLibIconPicker.returnFunc then
+		returnFunc( GetIconPath(self) );
+	end
+	EpsilonLibIconPicker.IconPath = GetIconPath(self);
+	if EpsilonLibIconPicker.playSound then
+		PlaySound(54129);
+	end
+	if EpsilonLibIconPicker.closeOnClick then
+		EpsilonLibIconPicker_Close();
+	end
+end
+
+-------------------------------------------------------------------------------
+-- OnEnter handler, to magnify the icon and show the texture path.
+--
+function EpsilonLibIconPickerButton_ShowTooltip( self )
+	GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
+	local texture = GetIconPath(self);
+	
+    GameTooltip:AddLine( "|T"..texture..":64|t", 1, 1, 1, true );
+    GameTooltip:AddLine( texture, 1, 0.81, 0, true );
+    GameTooltip:Show();
+end
+
+-------------------------------------------------------------------------------
+-- When the mousewheel is used on the icon map.
+--
+function EpsilonLibIconPicker_MouseScroll( delta )
+
+	local a = EpsilonLibIconPicker.selectorFrame.scroller:GetValue() - delta;
+	-- todo: do we need to clamp?
+	EpsilonLibIconPicker.selectorFrame.scroller:SetValue( a );
+end
+   
+-------------------------------------------------------------------------------
+-- When the scrollbar's value is changed.
+--
+function EpsilonLibIconPicker_ScrollChanged( value )
+	
+	-- Our "step" is ICONS_PER_LINE, which is one line.
+	startOffset = math.floor(value) * ICONS_PER_LINE;
+	EpsilonLibIconPicker_RefreshGrid();
+end
+
+-------------------------------------------------------------------------------
+-- Set the textures of the icon grid from the icons in the list at the
+-- current offset.
+--
+function EpsilonLibIconPicker_RefreshGrid()
+	local list = filteredList or EPSILONLIB_ICONS;
+	for k,v in ipairs( EpsilonLibIconPicker.icons ) do
+		local tex = list[startOffset + k];
+		if tex then
+			v:Show();
+			v:SetNormalTexture( tex );
+		else
+			v:Hide()
+		end
+	end
+end
+
+-------------------------------------------------------------------------------
+-- Called when the user types into the search box.
+--
+function EpsilonLibIconPicker_FilterChanged()
+	local filter = EpsilonLibIconPicker.search:GetText():lower();
+	if #filter < 3 then
+		-- Ignore filters less than three characters
+		if filteredList then
+			filteredList = nil;
+			EpsilonLibIconPicker_RefreshScroll();
+			EpsilonLibIconPicker_RefreshGrid();
+		end
+	else
+		-- build new list
+		filteredList = {}
+		for k,v in ipairs( EPSILONLIB_ICONS ) do
+			if v:lower():find( filter ) then
+				table.insert( filteredList, v );
+			end	
+		end
+		EpsilonLibIconPicker_RefreshScroll();
+	end
+end
+
+-------------------------------------------------------------------------------
+-- When we change the size of the list, update the scroll bar range.
+--
+-- @param reset Reset the scroll bar to the beginning.
+--
+function EpsilonLibIconPicker_RefreshScroll( reset )
+	local list = filteredList or EPSILONLIB_ICONS;
+	local max = math.floor((#list - MAX_ICONS) / ICONS_PER_LINE);
+	if max < 0 then max = 0 end		-- can't have negative max value here
+	EpsilonLibIconPicker.selectorFrame.scroller:SetMinMaxValues( 0, max );
+	
+	if reset then
+		EpsilonLibIconPicker.selectorFrame.scroller:SetValue( 0 );
+	end
+	
+	EpsilonLibIconPicker_ScrollChanged( EpsilonLibIconPicker.selectorFrame.scroller:GetValue() );
+end
+    
+-------------------------------------------------------------------------------
+-- Close the icon picker window. Use this instead of a direct Hide()
+--
+function EpsilonLibIconPicker_Close()
+	EpsilonLibIconPicker.returnFunc = nil;
+	EpsilonLibIconPicker.playSound = nil;
+	EpsilonLibIconPicker.closeOnClick = nil;
+	EpsilonLibIconPicker:Hide();
+end
+    
+-------------------------------------------------------------------------------
+-- Open the icon picker window.
+--
+-- @param returnFunc	If provided, clicking an icon will return its file path
+--						as a string to this function.
+--						If none provided, clicking an icon will instead set the 
+--						following global variables you can call instead:
+--
+--						EpsilonLibIconPicker.IconPath	string	The icon path.
+--
+-- @param closeOnClick	If true, the picker will close when an icon is clicked.
+-- @param playSound		If true, the picker will play a sound when an icon is
+--						clicked.
+--
+function EpsilonLibIconPicker_Open( returnFunc, closeOnClick, playSound )
+	filteredList = nil;
+	EpsilonLibIconPicker.returnFunc = returnFunc or nil;
+	EpsilonLibIconPicker.playSound = playSound or nil;
+	EpsilonLibIconPicker.closeOnClick = closeOnClick or nil;
+	
+	EpsilonLibIconPicker_RefreshScroll( true );
+	EpsilonLibIconPicker.search:SetText("");
+	EpsilonLibIconPicker:Show();
+end
+
+
---
+++ _retail_/Interface/AddOns/EpsilonLib/Utils/NineSliceTemplates.lua
@@ -0,0 +1,111 @@
+local EpsilonLib, EpsiLib = ...;
+
+EpsiLib.Utils.NineSlice = {}
+local NineSlice = EpsiLib.Utils.NineSlice
+
+--#region [[ Constants & Frame File References ]] --
+
+local addonPath = "Interface/AddOns/" .. tostring(EpsilonLib)
+
+local ASSETS_PATH = addonPath .. "/Resources/"
+
+local myNineSliceFile_corners = ASSETS_PATH .. "frame_border_corners"
+local myNineSliceFile_vert = ASSETS_PATH .. "frame_border_vertical"
+local myNineSliceFile_horz = ASSETS_PATH .. "frame_border_horizontal"
+
+--#endregion
+
+--#region [[ NineSlice Functions - Handles Setting Our Custom Layouts ]] --
+
+local function SetupTextureCoordinates(piece, setupInfo, pieceLayout, userLayout)
+	local left, right, top, bottom = 0, 1, 0, 1;
+	left = pieceLayout.txl or left
+	right = pieceLayout.txr or right
+	top = pieceLayout.txt or top
+	bottom = pieceLayout.txb or bottom
+
+	-- Propogate userLayout mirror setting to piece if piece does not define it
+	local pieceMirrored = pieceLayout.mirrorLayout;
+	if pieceMirrored == nil then
+		pieceMirrored = userLayout and userLayout.mirrorLayout;
+	end
+
+	if pieceMirrored then
+		if setupInfo.mirrorVertical then
+			top, bottom = bottom, top;
+		end
+
+		if setupInfo.mirrorHorizontal then
+			left, right = right, left;
+		end
+	end
+
+	piece:SetHorizTile(setupInfo.tileHorizontal);
+	piece:SetVertTile(setupInfo.tileVertical);
+	piece:SetTexCoord(left, right, top, bottom);
+end
+
+local function setupPieceVisualsFunction(container, piece, setupInfo, pieceLayout, textureKit, userLayout)
+	--- Change texture coordinates before applying atlas.
+	SetupTextureCoordinates(piece, setupInfo, pieceLayout, userLayout);
+
+	-- textureKit is optional, that's fine; but if it's nil the caller should ensure that there are no format specifiers in .atlas
+	--local atlasName = GetFinalNameFromTextureKit(pieceLayout.atlas, textureKit);
+	--local info = C_Texture.GetAtlasInfo(atlasName);
+	piece:SetHorizTile(pieceLayout and pieceLayout.tilesHorizontally or false);
+	piece:SetVertTile(pieceLayout and pieceLayout.tilesVertically or false);
+	piece:SetTexture(pieceLayout.tex, true, true);
+
+	local baseSize = 132
+	if pieceLayout.width then piece:SetWidth(pieceLayout.width) else piece:SetWidth(baseSize) end
+	if pieceLayout.height then piece:SetHeight(pieceLayout.height) else piece:SetHeight(baseSize) end
+end
+
+--#endregion
+
+--#region [[ Custom NineSlice Layouts ]] --
+
+-- Layout 1: EpsilonGoldBorderFrameTemplate
+NineSliceUtil.AddLayout("EpsilonGoldBorderFrameTemplate", {
+	setupPieceVisualsFunction = setupPieceVisualsFunction,
+	TopLeftCorner = { tex = myNineSliceFile_corners, txl = 0.263672, txr = 0.521484, txt = 0.263672, txb = 0.521484, x = -13, y = 16, },                  --0.263672, 0.521484, 0.263672, 0.521484
+	TopRightCorner = { tex = myNineSliceFile_corners, txl = 0.12890625, txr = 0.259766, txt = 0.263672, txb = 0.521484, x = 4, y = 16, width = 132 / 2 }, -- 0.00195312, 0.259766, 0.263672, 0.521484
+	BottomLeftCorner = { tex = myNineSliceFile_corners, txl = 0.00195312, txr = 0.259766, txt = 0.00195312, txb = 0.259766, x = -13, y = -3, },           -- 0.00195312, 0.259766, 0.00195312, 0.259766
+	BottomRightCorner = { tex = myNineSliceFile_corners, txl = 0.392578, txr = 0.521484, txt = 0.00195312, txb = 0.259766, x = 4, y = -3, width = 132 / 2 }, -- 0.263672, 0.521484, 0.00195312, 0.259766
+	TopEdge = { tex = myNineSliceFile_horz, txl = 0, txr = 1, txt = 0.263672, txb = 0.521484, },                                                          -- 0, 1, 0.263672, 0.521484
+	BottomEdge = { tex = myNineSliceFile_horz, txl = 0, txr = 1, txt = 0.00195312, txb = 0.259766, },                                                     -- 0, 1, 0.00195312, 0.259766
+	LeftEdge = { tex = myNineSliceFile_vert, txl = 0.00195312, txr = 0.259766, txt = 0, txb = 1, },                                                       -- 0.00195312, 0.259766, 0, 1
+	RightEdge = { tex = myNineSliceFile_vert, txl = 0.263672, txr = 0.521484, txt = 0, txb = 1, },                                                        -- 0.263672, 0.521484, 0, 1
+})
+
+-- Layout 1, alt variant (Double buttons in top-right): EpsilonGoldBorderFrameDoubleButtonTemplate
+local doubleButton = CopyTable(NineSliceUtil.GetLayout("EpsilonGoldBorderFrameTemplate"), true) -- Reuse the same layout as the main, modify TopRightCorner texcoord
+doubleButton.TopRightCorner = CopyTable(doubleButton.TopRightCorner)
+doubleButton.TopRightCorner.txt = 0.525391
+doubleButton.TopRightCorner.txb = 0.783203
+NineSliceUtil.AddLayout("EpsilonGoldBorderFrameDoubleButtonTemplate", doubleButton)
+
+
+-- Layout 2: EpsilonGoldBorderFrameTemplateNoPortrait -- Variant of Layout 1 without portrait
+NineSliceUtil.AddLayout("EpsilonGoldBorderFrameTemplateNoPortrait", {
+	setupPieceVisualsFunction = setupPieceVisualsFunction,
+	TopLeftCorner = { tex = myNineSliceFile_corners, txl = 0.525391, txr = 0.783203, txt = 0.00195312, txb = 0.259766, x = -12, y = 16, },
+	TopRightCorner = { tex = myNineSliceFile_corners, txl = 0.12890625, txr = 0.259766, txt = 0.263672, txb = 0.521484, x = 4, y = 16, width = 132 / 2 },
+	BottomLeftCorner = { tex = myNineSliceFile_corners, txl = 0.00195312, txr = 0.259766, txt = 0.00195312, txb = 0.259766, x = -12, y = -3, },
+	BottomRightCorner = { tex = myNineSliceFile_corners, txl = 0.392578, txr = 0.521484, txt = 0.00195312, txb = 0.259766, x = 4, y = -3, width = 132 / 2 },
+	TopEdge = { tex = myNineSliceFile_horz, txl = 0, txr = 1, txt = 0.263672, txb = 0.521484, tilesHorizontally = true },
+	BottomEdge = { tex = myNineSliceFile_horz, txl = 0, txr = 1, txt = 0.00195312, txb = 0.259766, tilesHorizontally = true },
+	LeftEdge = { tex = myNineSliceFile_vert, txl = 0.00195312, txr = 0.259766, txt = 0, txb = 1, tilesVertically = true },
+	RightEdge = { tex = myNineSliceFile_vert, txl = 0.263672, txr = 0.521484, txt = 0, txb = 1, tilesVertically = true },
+})
+
+-- Layout 2, alt variant (Double buttons in top-right): EpsilonGoldBorderFrameDoubleButtonTemplateNoPortrait
+local doubleButtonNoPortrait = CopyTable(NineSliceUtil.GetLayout("EpsilonGoldBorderFrameTemplateNoPortrait"), true) -- Reuse the same layout as the main, modify TopRightCorner texcoord
+doubleButtonNoPortrait.TopRightCorner = CopyTable(doubleButtonNoPortrait.TopRightCorner)
+doubleButtonNoPortrait.TopRightCorner.txt = 0.525391
+doubleButtonNoPortrait.TopRightCorner.txb = 0.783203
+NineSliceUtil.AddLayout("EpsilonGoldBorderFrameDoubleButtonTemplateNoPortrait", doubleButtonNoPortrait)
+
+
+
+-- You can add more custom templates if you want
