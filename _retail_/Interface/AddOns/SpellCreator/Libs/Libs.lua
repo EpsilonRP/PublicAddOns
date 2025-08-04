9a1a5b79e7581cb71e83cf47b285fa9aa8301f1c
MindScape <MindScape00@users.noreply.github.com>
2025-08-03 19:54:57 -0700

    SC / EL: Migrate Libraries

--- _retail_/Interface/AddOns/SpellCreator/Libs/Libs.lua
+++ _retail_/Interface/AddOns/SpellCreator/Libs/Libs.lua
@@ -22,7 +22,7 @@ if LibStub then
 	AceConfigDialog = LibStub:GetLibrary("AceConfigDialog-3.0")
 	AceConfigRegistry = LibStub:GetLibrary("AceConfigRegistry-3.0")
 	LibRPMedia = LibStub:GetLibrary("LibRPMedia-1.0");
-	LibBase64 = LibStub:GetLibrary("LibBase64-1.0");
+	--LibBase64 = LibStub:GetLibrary("LibBase64-1.0");
 end
 
 ---@class Libs
@@ -36,6 +36,6 @@ ns.Libs = {
 	AceSerializer = AceSerializer,
 	LibDeflate = LibDeflate,
 	LibRPMedia = LibRPMedia,
-	LibBase64 = LibBase64,
+	--LibBase64 = LibBase64,
 	Indent = {}, -- placeholder
 }
